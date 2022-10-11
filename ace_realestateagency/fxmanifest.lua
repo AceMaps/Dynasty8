0e0d7ebfb5913ebbcbcd3b06cd3a9ac2f58d55c3
Ace <104071730+AceMaps@users.noreply.github.com>
2022-10-11 19:15:25 +0200

    Add files via upload

---
+++ ace_realestateagency/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- For support join my discord: https://discord.gg/uZRXDbwpKA
+
+author "NaknoBaboch#5776"
+description "Blackmarket MLO"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map 'yes'
